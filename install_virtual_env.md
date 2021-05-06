93257090c37d32f9d6e59977e5b0b9617edd3523
Rick Staa <rick.staa@outlook.com>
2021-05-06 23:24:53 +0200

    :art: Fixes bugs in openai_ros package

---
+++ install_virtual_env.md
@@ -0,0 +1,9 @@
+# Create a virutal env
+
+You are recommended to use the openai_ros package inside a virtual env. This virtual env can be created using the following command:
+
+```python
+python -m venv ./openai_ros --system-site-packages
+```
+
+In this this the  `--system-site-packages` flag makes sure that the virtual environment has acces to the system site-packages. Alternatively you can also use the [RoboStack ros-noetic](https://github.com/RoboStack/ros-noetic) [conda-forge](https://conda-forge.org/) packages (see this [blog post](https://medium.com/robostack/cross-platform-conda-packages-for-ros-fa1974fd1de3) for more information).
---
+++ sandbox/roslaunch_test.py
@@ -0,0 +1,27 @@
+import roslaunch
+import rospy
+
+# Starting script
+# package = "turtlebot3_openai_example"
+# executable = "start_simulation.launch"
+# node = roslaunch.core.Node(package, executable)
+
+# launch = roslaunch.scriptapi.ROSLaunch()
+# launch.start()
+
+# process = launch.launch(node)
+# print(process.is_alive())
+# process.stop()
+
+## Starting launchfile
+
+rospy.init_node('test', anonymous=True)
+uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+roslaunch.configure_logging(uuid)
+launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/haier/catkin_ws/src/testapi/launch/test_node.launch"])
+launch.start()
+rospy.loginfo("started")
+
+rospy.sleep(3)
+# 3 seconds later
+launch.shutdown()

6c83a14233d7ddeae03b28da7fec4cb8ed0d7e2f
rickstaa <rick.staa@outlook.com>
2021-07-28 16:44:24 +0200

    :alien: Updates openai_ros and openai_example_projects submodules

---
+++ sandbox/test_dynamic_reconfigure.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+
+import dynamic_reconfigure.client
+
+
+def callback(config):
+    rospy.loginfo(config)
+
+
+if __name__ == "__main__":
+    rospy.init_node("dynamic_client")
+
+    client = dynamic_reconfigure.client.Client(
+        "dynamic_tutorials", timeout=30, config_callback=callback
+    )
+
+    r = rospy.Rate(0.1)
+    x = 0
+    b = False
+    while not rospy.is_shutdown():
+        x = x + 1
+        if x > 10:
+            x = 0
+        b = not b
+        client.update_configuration({})
+        r.sleep()

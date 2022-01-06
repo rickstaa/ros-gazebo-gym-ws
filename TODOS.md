09ffab20d659bd906d7e178d7896ce3734eb7de1
rickstaa <rick.staa@outlook.com>
2022-01-06 20:53:08 +0100

    chore: update submodules

--- TODOS.md
+++ TODOS.md
@@ -1,14 +1,11 @@
 # TODOS
 
 *   Rename start\_openai\_ros\_env module to make or REMOVE!.
-
-*   Check step rate.
-
-*   Profile code with austin to make it faster.
-
-*   Gazebo gravity compensation not working see https://github.com/frankaemika/franka\_ros/issues/160
+*   Put negative reward for collision.
+*   Add direct control.
+*   Log level gripper command
 
 # Open Upstream issues
 
-*   Gazeo torque calculation is wrong (https://github.com/frankaemika/franka\_ros/issues/160#issuecomment-961780423).
+*   Gazebo gravity compensation is wrong [see this issue](https://github.com/frankaemika/franka_ros/issues/160#issuecomment-961780423).
 *   Franka simulated gripper does not have the right PID gains (https://github.com/frankaemika/franka\_ros/issues/172#issuecomment-979254189). I currently applied [8114ed28630851cd09eed7c92bac51a70ef1ee03](https://github.com/rickstaa/franka_ros/commit/8114ed28630851cd09eed7c92bac51a70ef1ee03) to [rickstaa/franka\_ros/commits/develop-panda\_gazebo](https://github.com/rickstaa/franka_ros/commits/develop-panda_gazebo) as a hotfix. The grasp action and thus the pick and place environment does not yet work with this hotfix.
---
+++ sandbox/panda_training_freq_test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Small script that can be used to profile the training frequency.
+
+You can get the training frequency by checking the frequency of the
+`/panda_arm_joint1_effort_controller/command` topic. This is done using the
+`rostopic hz /panda_arm_joint1_effort_controller/command` command.
+"""
+
+import gym
+import numpy
+import rospkg
+import rospy
+import torch
+from openai_ros.core import start_openai_ros_env
+
+# Script parameters
+CONTROL_TYPE = "effort"
+TASK_ENV = "PandaReach-v0"
+N_STEPS = 1000
+N_EPISODES = 1
+
+if __name__ == "__main__":
+    rospy.init_node("panda_train_freq_test", anonymous=True, log_level=rospy.WARN)
+
+    # Init OpenAI_ROS ENV
+    env = start_openai_ros_env(TASK_ENV, control_type=CONTROL_TYPE)
+
+    # Create the Gym environment
+    rospy.loginfo("Gym environment done")
+    rospy.loginfo("Starting Learning")
+
+    # Set the logging system
+    rospack = rospkg.RosPack()
+    pkg_path = rospack.get_path("panda_openai_ros_example")
+    outdir = pkg_path + "/training_results"
+    last_time_steps = numpy.ndarray(0)
+
+    # Set max_episode_steps
+    env._max_episode_steps = N_STEPS
+
+    # Convert goal gym env to normal gym env
+    env = gym.wrappers.FlattenObservation(env)
+
+    # Perform simulated training loop
+    # NOTE: Here we use random actions and do not train a network
+    torch.cuda.empty_cache()
+    rospy.logwarn("Starting training loop")
+    obs = env.reset()
+    for x in range(N_EPISODES):
+        done = False
+        for i in range(N_STEPS):
+            action = env.action_space.sample()
+            obs, reward, done, info = env.step(action)
+            if done:
+                obs = env.reset()
+
+    env.close()
